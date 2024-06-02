66ad9320134ed2c06f648ec281eb9ec1dfbccd90
Complexlity <nwalozielijah@gmail.com>
2024-06-02 21:26:45 +0100

    fix: input too long

--- api/index.tsx
+++ api/index.tsx
@@ -46,7 +46,7 @@ app.frame('/', async (c) => {
     // image: "https://i.postimg.cc/Kv3j32RY/start.png",
     image: "https://i.postimg.cc/CxytCWs7/start.png",
     intents: [
-      <TextInput placeholder="Enter Contract Address e.g: 0xD77B108d4f6cefaa0Cae9506A934e825BEccA46E" />,
+      <TextInput placeholder="Enter Contract Address e.g: 0xD77B...ccA46E" />,
       <Button action="/token">Go</Button>,
     ],
   })
