14666d95391f38bd20e1f0babfaa3d07859f9a84
Complexlity <nwalozielijah@gmail.com>
2024-06-14 09:46:15 +0100

    chore: update image

--- api/index.tsx
+++ api/index.tsx
@@ -60,67 +60,59 @@ type StartFrameContext = FrameContext<
   },
   "/",
   BlankInput
-  >;
-
-
+>;
 
 async function handleTokenDetails(
   c: StartFrameContext,
   ca: string,
   method: string
 ) {
-  let token1: TokenDetails | null = null
-    let token2: TokenDetails | null = null
-
+  let token1: TokenDetails | null = null;
+  let token2: TokenDetails | null = null;
 
   if (method === "from") {
-    [token1, token2] = await Promise.all([getTokenPrice(), getTokenPrice(ca)])
+    [token1, token2] = await Promise.all([getTokenPrice(), getTokenPrice(ca)]);
     // if (!token1 || !token2) throw new Error("Could not get token1 or token2 from moralis")
     if (!token1 || !token2)
       return invalidOrMissingCaError(c, method, "Invalid Contract Address");
-    token1.tokenSymbol = 'ETH'
-  }
-
-  else {
-    [token1, token2] = await Promise.all([getTokenPrice(ca), getTokenPrice()])
+    token1.tokenSymbol = "ETH";
+  } else {
+    [token1, token2] = await Promise.all([getTokenPrice(ca), getTokenPrice()]);
     // if(!token1 || !token2) throw new Error("Could not get token1 or token2 from moralis")
     if (!token1 || !token2)
       return invalidOrMissingCaError(c, method, "Invalid Contract Address");
-    token2.tokenSymbol = 'ETH'
+    token2.tokenSymbol = "ETH";
   }
-
 
   const heading = getHeading(method);
 
-
-
   return c.res({
-    image: <S t1={token1} t2={token2}
-    />,
+    image: <S t1={token1} t2={token2} />,
     // image: dummyImage,
     intents: [
       <TextInput placeholder={`Amount (in ${token1.tokenSymbol})`} />,
       <Button value={method} action={`/confirm/${ca}`}>
         Proceed
       </Button>,
-      <Button value={method} action="/methods">Back</Button>,
+      <Button value={method} action="/methods">
+        Back
+      </Button>,
     ],
   });
 }
 async function invalidOrMissingCaError(
   c: StartFrameContext,
   method: string,
-  error: string,
+  error: string
 ) {
   const ethDetails = {
     tokenLogo: "https://i.ibb.co/Mg8Yd81/eth.png",
-    tokenSymbol: 'ETH'
-  }
+    tokenSymbol: "ETH",
+  };
 
-  console.log({method})
-  console.log({ error })
+  console.log({ method });
+  console.log({ error });
   const heading = getHeading(method);
-
 
   if (method === "from") {
     return c.res({
@@ -133,13 +125,12 @@ async function invalidOrMissingCaError(
         <Button action="/token" value="from">
           Proceed
         </Button>,
-        <Button value="from" action="/methods">Back</Button>,
+        <Button value="from" action="/methods">
+          Back
+        </Button>,
       ],
     });
-
-  }
-
-  else {
+  } else {
     return c.res({
       image: (
         //@ts-expect-error
@@ -150,13 +141,13 @@ async function invalidOrMissingCaError(
         <Button value="to" action="/token">
           Proceed
         </Button>,
-        <Button value="to" action="/methods">Back</Button>,
+        <Button value="to" action="/methods">
+          Back
+        </Button>,
       ],
     });
   }
 }
-
-
 
 // const dummyImage = "https://i.postimg.cc/Kv3j32RY/start.png";
 const dummyImage = "https://i.ibb.co/VYCmKgj/dummy-Image.jpg";
@@ -175,32 +166,31 @@ app.frame("/", analytics, async (c: StartFrameContext) => {
 });
 
 app.frame("/swap/:token1/:token2/:amount", async (c) => {
-
   const { inputText, url } = c;
-  let token1PriceData: TokenDetails | null = null
-  let token2PriceData: TokenDetails | null = null
-  let res: Response | null = null
-  let error: string | undefined = undefined
+  let token1PriceData: TokenDetails | null = null;
+  let token2PriceData: TokenDetails | null = null;
+  let res: Response | null = null;
+  let error: string | undefined = undefined;
 
-  let { token1, token2, amount } = c.req.param()
-  console.log({token1, token2, amount})
+  let { token1, token2, amount } = c.req.param();
+  console.log({ token1, token2, amount });
 
   if (token1 === "token1") token1 = inputText ?? "token1";
   else if (token2 === "token2") token2 = inputText ?? "token2";
   else amount = inputText ?? "amount";
 
-  console.log({ token1, token2, amount })
+  console.log({ token1, token2, amount });
   if (token1 === ETHEREUM_ADDRESS) {
-    token1 = 'token1'
-    error = 'Invalid contract address'
+    token1 = "token1";
+    error = "Invalid contract address";
   }
   if (token2 === token1) {
-    token2 = 'token2'
-    error = 'Both addresses must be different'
+    token2 = "token2";
+    error = "Both addresses must be different";
   }
   if (token2 === ETHEREUM_ADDRESS) {
-    token2 = 'token2'
-    error = "Invalid contract address"
+    token2 = "token2";
+    error = "Invalid contract address";
   }
   if (token1 === "token1") {
     console.log("Token 1 not defined");
@@ -209,17 +199,15 @@ app.frame("/swap/:token1/:token2/:amount", async (c) => {
       intents: [
         <TextInput placeholder="Token 1 CA" />,
         <Button action="/swap/token1/token2/ amount">Next</Button>,
-        <Button.Reset>Back</Button.Reset>
+        <Button.Reset>Back</Button.Reset>,
       ],
     });
   } else if (token2 === "token2") {
     token1PriceData = await getTokenPrice(token1);
-    if (!token1PriceData) throw new Error
-      ("Token 1 price data missing");
+    if (!token1PriceData) throw new Error("Token 1 price data missing");
     console.log("Token 2 not defined");
     return c.res({
-      image: <S t1={token1PriceData}
-      />,
+      image: <S t1={token1PriceData} />,
       intents: [
         <TextInput placeholder="Token 2 CA" />,
         <Button action={`/swap/${token1}/token2/amount`}>Next</Button>,
@@ -235,9 +223,7 @@ app.frame("/swap/:token1/:token2/:amount", async (c) => {
       throw new Error("Token 1 or Token 2 Missing");
     console.log("Amount not defined");
     return c.res({
-      image: (
-        <S t1={token1PriceData} t2={token2PriceData} />
-      ),
+      image: <S t1={token1PriceData} t2={token2PriceData} />,
       intents: [
         <TextInput placeholder={`Amount in ${token1PriceData.tokenSymbol}`} />,
         <Button action={`/swap/${token1}/${token2}/amount`}>Next</Button>,
@@ -246,17 +232,13 @@ app.frame("/swap/:token1/:token2/:amount", async (c) => {
     });
   }
 
-  if (token1 === "token1" || token2 === "token2" || amount === "amount") throw new Error("Not allowed")
-    let amountAsNumber = Number(amount);
-    if (
-      isNaN(amountAsNumber) ||
-      amountAsNumber == 0
-    ) {
-      amount = "1";
-      amountAsNumber = Number(amount);
-    }
-
-
+  if (token1 === "token1" || token2 === "token2" || amount === "amount")
+    throw new Error("Not allowed");
+  let amountAsNumber = Number(amount);
+  if (isNaN(amountAsNumber) || amountAsNumber == 0) {
+    amount = "1";
+    amountAsNumber = Number(amount);
+  }
 
   const action = `/approved/${token1}/${token2}/${amountAsNumber}`;
 
@@ -274,32 +256,35 @@ app.frame("/swap/:token1/:token2/:amount", async (c) => {
   [token1PriceData, token2PriceData, res] = await Promise.all([
     getTokenPrice(token1),
     getTokenPrice(token2),
-    fetcher
+    fetcher,
   ]);
-  if (!token1PriceData || !token2PriceData) throw new Error("Could not fetch data")
+  if (!token1PriceData || !token2PriceData)
+    throw new Error("Could not fetch data");
 
   if (!res || !res.ok) {
-    error = "Swap pool not found"
+    error = "Swap pool not found";
     return c.res({
-      image: (
-        <S t1={token1PriceData} t2={token2PriceData} />
-      ),
+      image: <S t1={token1PriceData} t2={token2PriceData} />,
       intents: [
         <TextInput placeholder={`Amount in ${token1PriceData.tokenSymbol}`} />,
         <Button action={`/swap/${token1}/${token2}/amount`}>Next</Button>,
         <Button action={`/swap/${token1}/${token2}/amount`}>Back</Button>,
       ],
     });
-
   }
   const priceData = (await res.json()) as ZeroxSwapPriceData;
   const tokenAmountReceived = Number(priceData.price) * amountAsNumber;
 
-
   return c.res({
     action,
-    image: <S
-      t1={token1PriceData} t2={token2PriceData} sA={amountAsNumber} rA={tokenAmountReceived} />,
+    image: (
+      <S
+        t1={token1PriceData}
+        t2={token2PriceData}
+        sA={amountAsNumber}
+        rA={tokenAmountReceived}
+      />
+    ),
     intents: [
       <Button.Transaction target={`/approve/${token1}`}>
         Approve
@@ -313,15 +298,13 @@ app.frame("/methods", async (c) => {
   const { buttonValue } = c;
   //No arguments = eth price
   let token = await getTokenPrice();
-  console.log(token?.tokenLogo)
-    if (!token) throw new Error("Token not found");
-   token.tokenSymbol = "ETH"
+  console.log(token?.tokenLogo);
+  if (!token) throw new Error("Token not found");
+  token.tokenSymbol = "ETH";
 
   if (buttonValue == "from") {
     return c.res({
-      image: <S
-        t1={token}
-      />,
+      image: <S t1={token} />,
       intents: [
         <TextInput placeholder="Enter Contract Address e.g: 0x.." />,
         <Button value="from" action="/token">
@@ -333,9 +316,7 @@ app.frame("/methods", async (c) => {
   }
 
   return c.res({
-    image: <S
-      t2={token}
-    />,
+    image: <S t2={token} />,
     intents: [
       <TextInput placeholder="Enter Token Address e.g: 0x.." />,
       <Button action="/token" value="to">
@@ -347,7 +328,7 @@ app.frame("/methods", async (c) => {
 });
 
 function getHeading(method: string) {
-  return method === "from" ? "Preview Purchase" : "Preview Sell"
+  return method === "from" ? "Preview Purchase" : "Preview Sell";
 }
 
 app.frame("/token", analytics, async (c: StartFrameContext) => {
@@ -355,11 +336,10 @@ app.frame("/token", analytics, async (c: StartFrameContext) => {
   let ca = inputText;
   let method = buttonValue;
 
-
   if (!method) method = "from";
   // if (!ca) ca = DEFAULT_TOKEN_CA;
-  console.log({ ca })
-    if (!ca) ca = DEFAULT_TOKEN_CA;
+  console.log({ ca });
+  if (!ca) ca = DEFAULT_TOKEN_CA;
 
   return handleTokenDetails(c, ca, method);
 });
@@ -392,28 +372,22 @@ app.frame("/confirm/:ca", analytics, async (c: StartFrameContext) => {
   let token2PriceData: TokenDetails | null = null;
   let res: Response | null = null;
 
-
-
   const params = new URLSearchParams({
     sellToken: token1,
     buyToken: token2,
     sellAmount: parseEther(tokenAmount).toString(),
   }).toString();
 
-
-
-
   // fs.writeFileSync("price.json", JSON.stringify(priceData, null, 2));
   const fetcher = fetch(baseUrl + params, {
     headers: { "0x-api-key": process.env.ZEROX_API_KEY || "" },
   });
 
-
   if (method === "from") {
     [token1PriceData, token2PriceData, res] = await Promise.all([
       getTokenPrice(),
       getTokenPrice(ca),
-      fetcher
+      fetcher,
     ]);
     if (!token1PriceData || !token2PriceData)
       throw new Error("Could not get token1 or token2 from moralis");
@@ -422,19 +396,16 @@ app.frame("/confirm/:ca", analytics, async (c: StartFrameContext) => {
     [token1PriceData, token2PriceData, res] = await Promise.all([
       getTokenPrice(ca),
       getTokenPrice(),
-      fetcher
+      fetcher,
     ]);
     if (!token1PriceData || !token2PriceData)
       throw new Error("Could not get token1 or token2 from moralis");
-
 
     token2PriceData.tokenSymbol = "ETH";
   }
 
   const priceData = (await res.json()) as ZeroxSwapPriceData;
   const tokenAmountReceived = Number(priceData.price) * tokenAmountAsNumber;
-
-
 
   const action =
     method === "from"
@@ -459,7 +430,13 @@ app.frame("/confirm/:ca", analytics, async (c: StartFrameContext) => {
       <Button.Transaction target={transactionTarget}>
         {method == "from" ? "Confirm" : "Approve"}
       </Button.Transaction>,
-      method == "from" ? <Button action={`/exact_token/${ca}`}>Back</Button> : <Button value="to" action="/methods">Cancel</Button>
+      method == "from" ? (
+        <Button action={`/exact_token/${ca}`}>Back</Button>
+      ) : (
+        <Button value="to" action="/methods">
+          Cancel
+        </Button>
+      ),
     ],
   });
 });
@@ -472,13 +449,11 @@ app.frame("/approved/:token1/:token2/:amount", async (c) => {
 
   console.log({ token1, token2, amount });
   if (!token1 || !amount) throw new Error("Token 1 not defined");
-    let amountAsNumber = Number(amount);
-    if (isNaN(amountAsNumber) || amountAsNumber == 0) {
-      amount = "1";
-      amountAsNumber = Number(amount);
-    }
-
-
+  let amountAsNumber = Number(amount);
+  if (isNaN(amountAsNumber) || amountAsNumber == 0) {
+    amount = "1";
+    amountAsNumber = Number(amount);
+  }
 
   const transactionTarget = `/sell/${token1}/${token2}/${amount}`;
   console.log({ transactionTarget });
@@ -554,8 +529,6 @@ app.frame("/finish", analytics, async (c: StartFrameContext) => {
     ],
   });
 });
-
-
 
 app.transaction("/approve/:ca", async (c) => {
   const ca = c.req.param("ca");
@@ -689,113 +662,113 @@ function S({
   sA: sA,
   rA: rA,
 }: {
-  t1?: TokenDetails,
-  t2?: TokenDetails,
-  sA?: number,
-  rA?: number,
-  }) {
-  const t1Logo = t1 ? t1.tokenLogo : dummyImage
+  t1?: TokenDetails;
+  t2?: TokenDetails;
+  sA?: number;
+  rA?: number;
+}) {
+  const t1Logo = t1 ? t1.tokenLogo : dummyImage;
   const t2Logo = t2 ? t2.tokenLogo : dummyImage;
   const t1Symbol = t1 ? t1.tokenSymbol : "??";
   const t2Symbol = t2 ? t2.tokenSymbol : "??";
-  const saValue = sA ? sA.toFixed(2) : "??"
+  const saValue = sA ? sA.toFixed(2) : "??";
   const raValue = rA ? rA.toFixed(2) : "??";
 
-    return (
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        fontSize: 32,
+        fontWeight: 600,
+        padding: "10px 50px",
+      }}
+      tw="bg-slate-900 text-white"
+    >
+      <span tw="text-6xl my-2 mx-auto ">Preview Swap</span>
       <div
+        tw="flex items-center  mx-auto justify-between max-w-4/5 w-full flex-col"
         style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          fontSize: 32,
-          fontWeight: 600,
-          padding: "10px 50px",
+          gap: "10px",
         }}
-        tw="bg-slate-900 text-white"
       >
-        <span tw="text-6xl my-4">Preview Swap</span>
-        <div
-          tw="flex items-center  mx-auto justify-between max-w-4/5 w-full flex-col"
-          style={{
-            gap: "10px",
-          }}
-        >
-          <div tw="flex justify-between py-2  w-full">
-            <div tw="rounded-full flex w-[100px] h-[100px] overflow-hidden ">
-              <img
-                src={t1Logo}
-                width={"100%"}
-                height={"100%"}
-                style={{
-                  objectFit: "cover",
-                }}
-              />
-            </div>
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="100"
-                height="100"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M18 8L22 12L18 16" />
-                <path d="M2 12H22" />
-              </svg>{" "}
-            </span>
-            <div tw="rounded-full flex w-[100px] h-[100px] overflow-hidden ">
-              <img
-                src={t2Logo}
-                width={"100%"}
-                height={"100%"}
-                style={{
-                  objectFit: "cover",
-                }}
-              />
-            </div>
-          </div>
-
-          <div tw="flex w-full justify-between px-4">
-            <span>{t1Symbol}</span>
-            <span>{t2Symbol}</span>
-          </div>
-        </div>
-        <hr tw="py-[1px] w-full bg-gray-800" />
-
-        <div tw="flex justify-between py-2">
-          <div tw="text-gray-400">You spend</div>
-          <div tw="flex text-4xl items-center" style={{ gap: "4px" }}>
-            {/* <img src={token.tokenLogo} width={50} height={50} /> */}
-            <span>You receive</span>
-          </div>
-        </div>
-        <div tw="flex justify-between py-2">
-          <span tw="text-gray-400 flex gap-2">{`${saValue} ${t1Symbol}`}</span>
-          <span tw="text-4xl flex" style={{ gap: "10px" }}>
-            <span>{raValue} </span>
-            <span>{t2Symbol}</span>
-          </span>
-        </div>
-
-        <div tw="flex justify-between py-2 items-center">
-          <span tw="text-gray-400">Chain</span>
-          <span style={{ gap: "4px" }} tw="flex items-center">
+        <div tw="flex justify-between py-4  w-full items-center">
+          <div tw="rounded-full flex w-[150px] h-[150px] overflow-hidden ">
             <img
-              src="https://i.ibb.co/BrQLkcw/arbitrum-arb-logo.png"
-              width={50}
-              height={50}
+              src={t1Logo}
+              width={"100%"}
+              height={"100%"}
+              style={{
+                objectFit: "cover",
+              }}
             />
-            <span>Arbitrum</span>
+          </div>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="100"
+              height="100"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M18 8L22 12L18 16" />
+              <path d="M2 12H22" />
+            </svg>{" "}
           </span>
+          <div tw="rounded-full flex w-[150px] h-[150px] overflow-hidden ">
+            <img
+              src={t2Logo}
+              width={"100%"}
+              height={"100%"}
+              style={{
+                objectFit: "cover",
+              }}
+            />
+          </div>
+        </div>
+
+        <div tw="flex w-full justify-between pl-12 pr-10">
+          <span tw="ml-2">{t1Symbol}</span>
+          <span>{t2Symbol}</span>
         </div>
       </div>
-    );
-  }
+      <hr tw="py-[1px] w-full bg-gray-800" />
+
+      <div tw="flex justify-between py-3">
+        <div tw="text-gray-400">You spend</div>
+        <div tw="flex text-4xl items-center" style={{ gap: "4px" }}>
+          {/* <img src={token.tokenLogo} width={50} height={50} /> */}
+          <span>You receive</span>
+        </div>
+      </div>
+      <div tw="flex justify-between py-3">
+        <span tw="text-gray-400 flex gap-2">{`${saValue} ${t1Symbol}`}</span>
+        <span tw="text-4xl flex" style={{ gap: "10px" }}>
+          <span>{raValue} </span>
+          <span>{t2Symbol}</span>
+        </span>
+      </div>
+
+      <div tw="flex justify-between py-2 items-center">
+        <span tw="text-gray-400">Chain</span>
+        <span style={{ gap: "4px" }} tw="flex items-center">
+          <img
+            src="https://i.ibb.co/BrQLkcw/arbitrum-arb-logo.png"
+            width={50}
+            height={50}
+          />
+          <span>Arbitrum</span>
+        </span>
+      </div>
+    </div>
+  );
+}
 function PreviewImage({
   method,
   amountReceived,
